8939d5f9bd647c148f16d62985f93ec235ad7163
Szymon Twardowski <szytwardowski@gmail.com>
2025-07-15 13:37:33 +0200

    error

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public boolean validate(String password, String repeatPassword) throws PasswordValidationException {
+    public boolean validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Passwords must not be null");
         }
